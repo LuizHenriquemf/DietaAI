b7f98fc356aed60b730f828c8f5405640eaf83cb
LuizHenriquemf <luizhenriquefurquim@hotmail.com>
2024-09-18 19:26:18 -0300

    Create Header

--- mobile/app/step/index.tsx
+++ mobile/app/step/index.tsx
@@ -6,7 +6,10 @@ import { Header } from '../../components/header'
 export default function Step() {
     return (
         <View>
-            <Header />
+            <Header
+                step='Passo 1'
+                title='Vamos começar'
+            />
         </View>
     )
 }
